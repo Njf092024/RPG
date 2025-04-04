7c9d5a21641b0c0d9ce11a5e37586eff72565731
Njf092024 <Njf092024@gmail.com>
2025-04-04 18:35:25 +0200

    Add Animator reference to script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
 {
     public float speed = 5;
     public Rigidbody2D rb;
+    public Animator anim;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
