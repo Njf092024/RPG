759762e0081955579ea635dd689773f495311949
Njf092024 <Njf092024@gmail.com>
2025-04-09 12:35:41 +0200

    Add trigger logic to disable mountainColliders on entry

--- Assets/Scripts/Tilemap Scripts/Elevation_Entry.cs
+++ Assets/Scripts/Tilemap Scripts/Elevation_Entry.cs
@@ -4,4 +4,12 @@ public class Elevation_Entry : MonoBehaviour
 {
 
     public Collider2D[] mountainColliders;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        foreach (Collider2D mountain in mountainColliders)
+        {
+            mountain.enabled = false;
+        }
+    }
 }
